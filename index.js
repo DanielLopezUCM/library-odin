3ded715679d06c5b3d468d2701fe91080c7bf7fa
DanielLopezUCM <117196575+DanielLopezUCM@users.noreply.github.com>
2023-10-19 20:32:51 +0200

    Commit Pre-recovery

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const myLibrary = [];
 
 function createBook(title, author, pages, read) {
-    const getName = () => title;
+    const getTitle = () => title;
     const getAuthor = () => author;
     const getPages = () => pages;
     const isRead = () => read;
@@ -9,12 +9,53 @@ function createBook(title, author, pages, read) {
         read = value;
     }
 
-    return { getName, getAuthor, getPages, isRead, setRead };
+    return { getTitle, getAuthor, getPages, isRead, setRead };
 }
 
 function addBookToLibrary(name, author, pages, read) {
     const book = createBook(name, author, pages, read);
+    const bookCard = createHTMLBook(book);
+    const booksGrid = document.getElementById("books-grid");
+    booksGrid.insertBefore(bookCard, booksGrid.lastChild.previousSibling);
     myLibrary.push(book);
+}
+
+function createHTMLBook(bookData) {
+    const book = document.createElement("div");
+    book.className = "book-card grid-elem";
+
+    const info = document.createElement("div");
+    info.className = "book-data";
+    book.appendChild(info);
+
+    const title = document.createElement("div");
+    title.innerText = `"${bookData.getTitle()}"`;
+    title.className = "book-title";
+    info.appendChild(title);
+
+    const author = document.createElement("div");
+    author.innerText = bookData.getAuthor();
+    author.className = "book-author";
+    info.appendChild(author);
+
+    const nPages = bookData.getPages();
+    const pages = document.createElement("div");
+    pages.innerText = `${nPages} page${nPages > 1 ? 's' : null}`;
+    pages.className = "book-pages";
+    info.appendChild(pages);
+
+    const readButton = document.createElement("button");
+    readButton.onclick = () => {
+        bookData.setRead(!bookData.isRead());
+        book.classList.replace(bookData.isRead() ? "not-read" : "read", bookData.isRead() ? "read" : "not-read");
+        readButton.innerText = bookData.isRead() ? "Not read" : "Read";
+    };
+    book.classList.add(bookData.isRead() ? "read" : "not-read");
+    readButton.className = "book-button";
+    readButton.innerText = bookData.isRead() ? "Not read" : "Read";
+    book.appendChild(readButton);
+
+    return book;
 }
 
 function clearBooksGrid() {
@@ -23,3 +64,5 @@ function clearBooksGrid() {
     grid.innerHTML = "";
     grid.appendChild(addButton);
 }
+
+addBookToLibrary("Prueba", "Dani", 2, false);
